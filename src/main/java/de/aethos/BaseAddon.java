9c6a0eebe56bb5e2e7267ab28ee6ddecca78c99e
Tyrael <xykingofskillyx@gmail.com>
2024-06-06 00:14:29 +0200

    Initial Commit x2

--- src/main/java/de/aethos/BaseAddon.java
+++ src/main/java/de/aethos/BaseAddon.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class BaseAddon {
+package de.aethos;
+
+import de.aethos.skills.AethosSkillAPI;
+import de.aethos.skills.addon.Addon;
+
+public class BaseAddon extends Addon {
+    public void onEnabled(AethosSkillAPI plugin){
+        System.out.println("BaseAddon enabled");
+    }
+
+    public void onDisabled(AethosSkillAPI plugin){
+        System.out.println("BaseAddon disabled");
+    }
+
 }
